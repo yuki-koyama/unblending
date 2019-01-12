1520ae587813bc5d4b86d2519a0c63f776f3ded0
Yuki Koyama <yuki@koyama.xyz>
2019-01-12 21:55:34 +0900

    Add comments

--- unblending/include/unblending/unblending.hpp
+++ unblending/include/unblending/unblending.hpp
@@ -9,11 +9,13 @@
 
 namespace unblending
 {
+    /// \brief Compute the main unblending optimization
     std::vector<ColorImage> compute_color_unmixing(const ColorImage& image,
                                                    const std::vector<LayerInfo>& layer_infos,
                                                    const bool has_opaque_background,
                                                    const int target_concurrency = 0);
     
+    /// \brief Compute the sub unblending optimization for refinement
     std::vector<ColorImage> perform_matte_refinement(const ColorImage&              image,
                                                      const std::vector<ColorImage>& layers,
                                                      const std::vector<LayerInfo>&  layer_infos,
@@ -21,10 +23,12 @@ namespace unblending
                                                      const bool                     force_smooth_background,
                                                      const int                      target_concurrency = 0);
     
+    /// \brief Calculate a blended image from multiple layers by color blending
     ColorImage composite_layers(const std::vector<ColorImage>& layers,
                                 const std::vector<CompOp>&     comp_ops,
                                 const std::vector<BlendMode>&  modes);
     
+    /// \brief Export layers as image files
     void export_layers(const std::vector<ColorImage>& layers,
                        const std::string&             output_directory_path,
                        const std::string&             file_name_prefix,
@@ -32,13 +36,16 @@ namespace unblending
                        const bool                     with_blend_mode_suffix = false,
                        const std::vector<LayerInfo>&  layer_infos            = {});
     
+    /// \brief Export color models as image files
     void export_models(const std::vector<ColorModelPtr>& models,
                        const std::string& output_directory_path,
                        const std::string& file_name_prefix);
     
+    /// \brief Export layer infos as a JSON file
     void export_layer_infos(const std::vector<LayerInfo>& layer_infos,
                             const std::string& output_directory_path);
     
+    /// \brief Import layer infos from a JSON file
     std::vector<LayerInfo> import_layer_infos(const std::string& input_file_path);
 }
 
